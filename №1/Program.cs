561cc64deeba4602c64ed848e5f1fa17fa8fe2a2
Kirill <kirillnaumenk0@mail.ru>
2022-08-20 10:12:31 +0600

    commit №1

---
+++ №1/Program.cs
@@ -0,0 +1,46 @@
+﻿class Program
+{
+
+    static int[] CreaterArray(int number)
+    {
+        int[] array = new int[number];
+        for (int i = 0; i < array.Length; i++)
+        {
+            array[i] = new Random().Next(300, 999);
+        }
+        return array;
+    }
+
+
+    static void WriteArray(int[] array)
+    {
+        Console.WriteLine("Вывод массива");
+        for (int i = 0; i < array.Length; i++)
+        {
+            Console.Write(array[i] + " ");
+        }
+    }
+
+    static void MultipleTwoNumber(int[] array)
+    {
+        int s = 0;
+        for (int i = 0; i < array.Length; i++)
+        {
+            if (array[i] % 2 == 0)
+            {
+                s++;
+            }
+        }
+        Console.WriteLine(" ");
+        Console.WriteLine("Количество чётных чисел в массиве: " + s);
+    }
+
+
+
+    static void Main(string[] args)
+    {
+        int[] array = CreaterArray(10);
+        WriteArray(array);
+        MultipleTwoNumber(array);
+    }
+}
